cdbab8c99af64a71f5d0efa02a0a8610234093f1
Xavi Navarro <xnavarroiguacel@gmail.com>
2021-05-11 19:10:59 +0200

    initial commit

---
+++ components/Button.vue
@@ -0,0 +1,16 @@
+<template>
+<button :class="btnClass" @click="click">
+    {{btnText}}
+</button>
+    
+</template>
+
+<script>
+export default {
+    props:['btnText','btnClass',"click"]
+}
+</script>
+
+<style>
+
+</style>
---
+++ components/Logo.vue
@@ -0,0 +1,20 @@
+<template>
+ 
+   <img class="logo" src="~/assets/Metamask_Fox.svg" />
+
+  
+</template>
+
+<style>
+.logo {
+  animation: 1s appear;
+  margin: auto;
+  width:300px;
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+}
+</style>
